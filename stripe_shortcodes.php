6212064c9e788c22ce2f456fae24408d608bc38e
Adan <adangrx@gmail.com>
2020-04-27 22:53:13 +0100

    Update problem with capabilities

--- stripe_shortcodes.php
+++ stripe_shortcodes.php
@@ -1,27 +1,12 @@
 <?php
-/*--------------------------------------------------------------
-# Save billing information on database Change the ID form on gform_after_submission_ID
---------------------------------------------------------------*/
-add_action("gform_after_submission_7", "activate_subscription_employer", 10, 2);
 
-function activate_subscription_employer ($entry, $form) {
-	//save the entry id to the current users meta when they subscribe for premium stuff
-	update_user_meta($entry['created_by'], 'snp_entry_id', $entry['id']);
-	
-	//Save billing information
-	if( isset(update_user_meta(get_current_user_id(), '_address1', $entry['5.1']) ));
-	if(isset(update_user_meta( get_current_user_id(), '_address2', $entry['5.2']) ));
-	if(isset(update_user_meta( get_current_user_id(), '_city', $entry['5.3']) ));
-	if(isset(update_user_meta( get_current_user_id(), '_state', $entry['5.4']) ));
-	if(isset(update_user_meta( get_current_user_id(), '_zipcode', $entry['5.5']) ));
-	if(isset(update_user_meta( get_current_user_id(), '_country', $entry['5.6']) ));
-}
+
 
 /*--------------------------------------------------------------
 # Unsubscription and table function and shortcode
 --------------------------------------------------------------*/
 function get_all_subscriptions($id, $user_id = null) {
-	$id=7;
+	$id=14;
 	$search_criteria2['field_filters'][] = array( 'key' => 'created_by', 'value' => $user_id );			
 	$sorting = array();
 	$paging = array( 'offset' => 0, 'page_size' => 30 );
@@ -32,6 +17,7 @@ function get_all_subscriptions($id, $user_id = null) {
 	foreach ($form_entries as $entry){
 		$result = array();
 		$feed = gf_stripe()->get_payment_feed( $entry );
+		
 		$subscription_name = $feed['meta']['subscription_name'];
 		$entry_id = $entry['id'];
 		$status = $entry['payment_status'];
@@ -40,14 +26,16 @@ function get_all_subscriptions($id, $user_id = null) {
 		$format_date = date('m/d/y', $start_date);
 
 		$length = $feed['meta']['billingCycle_length']." ".$feed['meta']['billingCycle_unit'];
-			$result['lit_entry_id'] = $entry_id;
-			$result['lit_subscription_name'] = $subscription_name;
-			$result['lit_date'] = $format_date;
-			$result['lit_amount'] = $amount;
-			$result['lit_status'] = $status;
-			
-			$results[$entry_id] = $result;
+
+		$result['lit_entry_id'] = $entry_id;
+		$result['lit_subscription_name'] = $subscription_name;
+		$result['lit_date'] = $format_date;
+		$result['lit_amount'] = $amount;
+		$result['lit_status'] = $status;
+		
+		$results[$entry_id] = $result;
 	}
+	
 	return  $results;
 }
 
@@ -56,8 +44,7 @@ function get_all_subscriptions($id, $user_id = null) {
 # Shortcode
 --------------------------------------------------------------*/
 function get_all_subscriptions_shortcode($id = 0){
-		
-
+	
 		ob_start();
 		
 		$results = get_all_subscriptions($id, get_current_user_id());
@@ -72,16 +59,41 @@ function get_all_subscriptions_shortcode($id = 0){
 		wp_enqueue_script('stripe_2_js', get_stylesheet_directory_uri() . '/js/stripe/dataTables.responsive.min.js', array('jquery') );
 		wp_enqueue_script('stripe_3_js', get_stylesheet_directory_uri() . '/js/stripe/custom.js', array('jquery') );
 
-		if ($status == 'Active'){
-			echo  '<a class="cancel_subscription_popup" style="border-bottom: 2px solid !important; width: 100%; display: block; margin: 20px 0px;" href="#">Cancel my subscription</a>';
-			echo 'Your Employer subscription is active or subscription period not end';
-		} else{
-			echo '<div class="upgrade_button">
-			<a class="subscription-employer-pro" href="#" style="border-bottom: 2px solid !important;">Change/Upgrade my role</a>
-			<strong><span>Employer Pro Subscription status:</span></strong> ' . $status . 
-			'</div>';
+		$subscription_plan = get_user_meta( get_current_user_id(), '_subscription_plan', true );
+		
+		
+	
+		if($subscription_plan){ 
+			echo '<div>Role acutal: ' . '<strong>' . $subscription_plan . '</strong></div>';
+		}else {
+			$user_role = get_current_user_id();
+			$user = get_userdata($user_role);
+			$user_roles=$user->roles;
+				echo '<div>Role acutal: ' . '<strong>' . $user_roles[0] . '</strong></div>';
 		}
 		
+		if ($status == false){
+			echo '<div><span>Employer Pro Subscription renew status: </span> <strong>Inactive</strong></div>';
+ 		}
+
+ 		if ($status == true){
+			echo '<div><span>Employer Pro Subscription renew status: </span> <strong>Active</strong></div>';
+		} 
+		
+		if ($subscription_plan == 'Employer Recruiter Pro'){
+			
+			$date = reset($results)['lit_date'];
+			$start = new DateTime($date, new DateTimeZone("UTC"));
+			$month_later = clone $start;
+			$month_later->add(new DateInterval("P1M"));
+			echo 'Your Subscription ends: ' . '<strong>' . $month_later->format('m/d/Y') . '</strong>';
+			
+		}
+			
+		echo '<div class="upgrade_button">
+				<a class="subscription-employer-pro" href="#" style="border-bottom: 2px solid !important;">Change/Upgrade my role</a>
+			</div>';
+				
 		$html = "<div class='table-responsive'>";
 			$html .= "<table id='user-subscriptions' class='table dt-responsive' cellspacing='0' width='100%'>";
 			$html .= ' <thead>
@@ -95,7 +107,6 @@ function get_all_subscriptions_shortcode($id = 0){
 			<tbody>';
 
 		foreach ($results as $result){
-			
 			$html .= "<tr class='gss_item'>";				
 				$html .= "<td class='name'>".$result['lit_subscription_name']."</td>";
 				$html .= "<td class='date'>".$result['lit_date']."</td>";
@@ -105,6 +116,15 @@ function get_all_subscriptions_shortcode($id = 0){
 		}
 		$html .= "</tbody></table></div>";
 		echo $html;
+		
+		
+		
+			
+		if ($status == true){
+			echo '<div style="text-align: right"><a class="cancel_subscription_popup" href="#">Cancel my subscription</a></div>';
+		}
+		
+		
 				
 	return ob_get_clean();
 }
--- stripe_subscription.php
+++ stripe_subscription.php
@@ -1,18 +1,42 @@
 <?php
-	
-/* ################ stripe shortocodes ################################# */
-
+/*--------------------------------------------------------------
+# Include Stripe Shortcodes
+--------------------------------------------------------------*/
 require_once( trailingslashit (get_stylesheet_directory() ). '/stripe/stripe_shortcodes.php');
-	
 
-/*----------------------------------------------------------------------------------------------------------------------
-# Start stripe logic here --
-----------------------------------------------------------------------------------------------------------------------*/
+	
+/*--------------------------------------------------------------
+# Save billing information on database
+--------------------------------------------------------------*/
+add_action("gform_after_submission_14", "activate_subscription_employer", 10, 2);
+
+function activate_subscription_employer ($entry, $form) {
+   $post_id = $entry["id"];
+
+   //save the entry id to the current users meta when they subscribe for premium stuff
+   update_user_meta($entry['created_by'], 'snp_entry_id', $entry['id']);
+   
+   //Save billing information
+   update_user_meta(get_current_user_id(), '_address1', $entry['5.1']);
+   update_user_meta( get_current_user_id(), '_address2', $entry['5.2']);
+   update_user_meta( get_current_user_id(), '_city', $entry['5.3']);
+   update_user_meta( get_current_user_id(), '_state', $entry['5.4']) ;
+   update_user_meta( get_current_user_id(), '_zipcode', $entry['5.5']) ;
+   update_user_meta( get_current_user_id(), '_country', $entry['5.6']) ;
+   
+}
+
+
+/*----------------------------------------------------------------
+# Execute after payments
+------------------------------------------------------------------*/
 
 function pwm_after_payment( $entry, $action ) {
+	
 	$entry_id = rgar( $entry, 'id' );
 	//if user is logged in save the stripe customer id for new subscriptions and save more info to users meta
 	if ( rgar( $action, 'type' ) == 'create_subscription' ) {
+
 		$stripe_customer_id = rgar( $action, 'customer_id' );
 		if ( is_user_logged_in() ) {
 			$user_id = get_current_user_id();
@@ -20,17 +44,21 @@ function pwm_after_payment( $entry, $action ) {
 			if ( get_user_meta( $user_id, 'stripe_customer_id', true ) == '' ) {
 				update_user_meta( $user_id, 'stripe_customer_id', $stripe_customer_id );
 			}
+			
 			pwm_add_subscription( $entry_id, $user_id );			
 		}
-		//if user is not logged in we will need to get the user id from user registered hook. not sure if that runs
-		//before this. So just save the stripe ID from this hook to use in that hook.
+		//if user is not logged in we will need to get the user id from user registered hook.
 		gform_update_meta( $entry_id, 'pwm_stripe_customer_id', $stripe_customer_id );
+
 	}
 }
 add_action( 'gform_post_payment_action', 'pwm_after_payment', 10, 2 );
 
 
 
+/*----------------------------------------------------------------
+# Add Subscription to database info after payment
+------------------------------------------------------------------*/
 
 /**
  * @param $entry_id
@@ -53,20 +81,20 @@ function pwm_add_subscription( $entry_id, $user_id = 0 ) {
 	update_user_meta( $user_id, 'pwm_subscription_entry_id', $entry_id );
 	update_user_meta( $user_id, 'pwm_subscribed_before', true );
 	update_user_meta( $user_id, 'pwm_subscribe_date', date( 'Y-m-d' ) ); //good to have :)
-	update_user_meta( $user_id, '_subscription_status', "Active" );
+	update_user_meta( $user_id, '_subscription_status', 1 );
+	update_user_meta( $user_id, '_subscription_plan', 'Employer Recruiter Pro' );
 
 	//when cancelling payment we dont know who were deleting from! sometimes there is $entry['created_by'] sometimes not. This is for sure.
 	gform_update_meta( $entry_id, 'pwm_subscribers_user_id', $user_id );
 
 	//upgrade user
 	pwm_set_user_role( $user_id, 'employer-recruiter-50');
-	
 }
 
 
-/**
- * Cancel at period end AND dont run the callback the first time when this runs via the subscribe or renew feed
- */
+/*----------------------------------------------------------------
+# Cancel at period end AND dont run the callback the first time when this runs via the subscribe or renew feed
+------------------------------------------------------------------*/
 add_filter( 'gform_stripe_subscription_cancel_at_period_end', 'stripe_subscription_cancel_at_period_end', 10, 3 );
 function stripe_subscription_cancel_at_period_end( $at_period_end, $entry, $feed ) {
 	
@@ -76,43 +104,51 @@ function stripe_subscription_cancel_at_period_end( $at_period_end, $entry, $feed
 		remove_action( 'gform_post_payment_callback', 'cancelling_at_period_end' );
 		return true;
 	}
+
 	//return $at_period_end;
 	return true;
 }
 
+
+/*----------------------------------------------------------------
+# Runs at actual period end. remove paid privileges and other stuff
+------------------------------------------------------------------*/
 /**
  * @param $entry
  * @param $action
  * @param $result
- *
- * Runs at actual period end. remove paid privileges and other stuff
  */
 function cancelling_at_period_end( $entry, $action, $result ) {
+	//write_log('running cancellation callback');
 	if ( ! $result && rgar( $action, 'type' ) == 'cancel_subscription' && strtolower( $entry['payment_status'] ) == 'cancelled'	) {
 		//end of subscription has come.
 		$entry_id = rgar( $entry, 'id' );
-		$user_id = gform_get_meta( $entry_id, 'pwm_subscribers_user_id' );		
+		$user_id = gform_get_meta( $entry_id, 'pwm_subscribers_user_id' );
 		pwm_remove_subscription($entry_id, $user_id);
 	}
 }
 add_action( 'gform_post_payment_callback', 'cancelling_at_period_end', 10, 3 );
 
 
+/*----------------------------------------------------------------
+# Update the subscrive till end and change role method
+------------------------------------------------------------------*/
 /**
  * @param $entry_id
  * @param $user_id
- * Anything you want when the subscription truly ends
  */
 function pwm_remove_subscription($entry_id, $user_id ){
 	update_user_meta( $user_id, 'pwm_subscribed_till_end', '' );
+	
 	//downgrade role if necessary. make sure its not an admin!
 	pwm_remove_user_role( $user_id,  'employer-recruiter-50' );
+
 }
 
 
-/**
- * get the right customer id if logged in when making a payment, if exists, dont make a new one
- */
+/*----------------------------------------------------------------
+# Get the right customer id if logged in when making a payment, if exists, dont make a new one
+------------------------------------------------------------------*/
 add_filter( 'gform_stripe_customer_id', 'get_stripe_customer_id', 10, 4 );
 function get_stripe_customer_id( $customer_id, $feed, $entry, $form ) {
 	$feed_name = rgars( $feed, 'meta/feedName' );
@@ -125,18 +161,10 @@ function get_stripe_customer_id( $customer_id, $feed, $entry, $form ) {
 }
 
 
-
-
 /*--------------------------------------------------------------
-# Unsubscribe user form
+# Cancel Subscription start
 --------------------------------------------------------------*/
-//logged in user can cancel subscription with a cancel form.
-add_action( 'gform_after_submission_6', 'pwm_cancel_subscription', 10, 2 );
-function pwm_cancel_subscription( $entry, $form ) {
-
-	//SET SUBSCRIPTION TO CANCEL
-	//get original current users entry id from subscription form.
-
+function cancel_subscription_manually(){
 	$entry_id = get_user_meta( get_current_user_id(), 'pwm_subscription_entry_id', true );
 	if ( $entry_id > 0 ) {
 		//now cancel that old entry's subscription
@@ -150,22 +178,80 @@ function pwm_cancel_subscription( $entry, $form ) {
 			update_user_meta( get_current_user_id(), 'pwm_subscription_entry_id', '' );
 			//set them to subscribed till period ends. in limbo state.
 			update_user_meta( get_current_user_id(), 'pwm_subscribed_till_end', true );
+			
+			//Update subscription status
+			update_user_meta( get_current_user_id(), '_subscription_status', 0 );
 		}
+		//Update subscription status
+		update_user_meta( get_current_user_id(), '_subscription_status', 0 );
+	}	
+	
+	//Update subscription status
+	update_user_meta( get_current_user_id(), '_subscription_status', 0 );
+}
+
+
+
+
+/*----------------------------------------------------------------
+# Update billing information alone
+------------------------------------------------------------------*/
+//when updating billing make sure they are not being charged this time, just updating customers billing info
+//create form with product of 1.00 hidden. not subscription. then set feed name to Update Credit Card.
+/*
+add_filter( 'gform_stripe_charge_authorization_only', 'stripe_charge_authorization_only', 10, 2 );
+function stripe_charge_authorization_only( $authorization_only, $feed ) {
+	//For plugin check if this is for update billing
+	$feed_name = rgars( $feed, 'meta/feedName' );
+	if ( $feed_name == 'Update Credit Card' ) {
+		return true;
 	}
 
+	return $authorization_only;
+}
+*/
+
+
+
+
+/*--------------------------------------------------------------
+# Unsubscribe user form
+--------------------------------------------------------------*/
+//logged in user can cancel subscription with a cancel form.
+add_action( 'gform_after_submission_13', 'pwm_cancel_subscription', 10, 2 );
+function pwm_cancel_subscription( $entry, $form ) {
+	//Cancel subscription method
+	cancel_subscription_manually();
 }
 
+
+/*--------------------------------------------------------------
+# Remove user role and change the subscription plan , Sets the role for the user as specified in the function.
+--------------------------------------------------------------*/
+/**
+ * @param int $user_id 
+ * @param string $role
+ */
 function pwm_remove_user_role($user_id, $role){
+	
 	$employer_pro = get_userdata($user_id);
 	$employer_pro->remove_role($role); 	
-	update_user_meta( $user_id, '_subscription_status', "Inactive" );
+
+	$subscription_plan = get_user_meta( $user_id, '_subscription_plan', true );
+	
+	if ($subscription_plan == 'Employer Recruiter Pro'){
+		update_user_meta( $user_id, '_subscription_plan', 'Employer Free' );
+	}
+
+	update_user_meta( $user_id, '_subscription_status', 0 );
 }
 
 
-
+/*--------------------------------------------------------------
+# Sets the role for the user as specified in the function.
+--------------------------------------------------------------*/
 /**
- * Sets the role for the user as specified in the function.
- * @param    int $user_id The ID of the user for whom we're updating the role.
+ * @param int $user_id 
  */
  
 function pwm_set_user_role( $user_id, $role ) {
@@ -173,41 +259,101 @@ function pwm_set_user_role( $user_id, $role ) {
 	$user = new \WP_User( $user_id );
 	$user_roles=$user->roles;
 
-	if (in_array('candidate', $user_roles)){
-		$user->remove_role('candidate');
-	}
-
-	if (in_array('supporter', $user_roles)){
-		$user->remove_role('supporter');
-	}
+	if ( is_user_logged_in() ) {
+		if (in_array('candidate', $user_roles)){
+			$user->remove_role('candidate');
+		}
+		
+		if (in_array('supporter', $user_roles)){
+			$user->remove_role('supporter');
+		}
 	
-	$user->add_role('employer');
-	$user->add_role( $role );	
+		$user->add_role('employer');
+		$user->add_role( $role );	
+	}
 }
 
 
 
 
+/*--------------------------------------------------------------
+# Control change role form, and change the role if no start premium. Only for free users
+--------------------------------------------------------------*/
 
-/* Other Forms */
 function manage_my_subscription($entry, $form){	
+	
 	$selectedItem = rgar( $entry, '7' );
-	$user = get_userdata(get_current_user_id());
+	
+	$user_id = get_current_user_id();
+	$user = get_userdata($user_id);
 	$user_roles=$user->roles;
 	
-	switch ($selectedItem) {
-	    case strpos($selectedItem, 'supporter'):
-	        $selectedItem = 'supporter';
-	        $user->remove_role('candidate');
-	        $user->remove_role('employer');
-	        break;
-	    case strpos($selectedItem, 'candidate'):
-	        $selectedItem = 'candidate';
-	        $user->remove_role('supporter');
-			$user->remove_role('employer');
-	        break;
-	}	
-	$user->add_role($selectedItem);	
+	if ( is_user_logged_in() ) {
+			switch ($selectedItem) {
+			    case strpos($selectedItem, 'supporter'):
+			        $selectedItem = 'supporter';
+			        $user->remove_role('employer-recruiter-50');
+			        $user->remove_role('candidate');
+			        $user->remove_role('employer');
+			        update_user_meta( $user_id, '_subscription_plan', 'Ally' );
+			        cancel_subscription_manually();
+			        $user->add_role($selectedItem);	
+			        break;
+			        
+			    case strpos($selectedItem, 'candidate'):
+			        $selectedItem = 'candidate';
+			        $user->remove_role('employer-recruiter-50');
+			        $user->remove_role('supporter');
+					$user->remove_role('employer');
+					update_user_meta( $user_id, '_subscription_plan', 'Latinas in Tech' );
+					cancel_subscription_manually();
+					$user->add_role($selectedItem);	
+			        break;
+			        
+			    case strpos($selectedItem, 'employer-free'):
+			        $selectedItem = 'employer';
+			        $user->remove_role('employer-recruiter-50');
+			        $user->remove_role('supporter');
+					$user->remove_role('candidate');
+					update_user_meta( $user_id, '_subscription_plan', 'Employer Free' );
+					cancel_subscription_manually();
+					$user->add_role($selectedItem);	
+			        break;
+	/*
+			    case strpos($selectedItem, 'employer-recruiter-50|50'):
+			        $selectedItem = 'employer-recruiter-50';
+			        $user->remove_role('supporter');
+					$user->remove_role('candidate');
+					update_user_meta( $user_id, '_subscription_plan', 'Employer Recruiter Pro' );
+					cancel_subscription_manually();
+					$user->add_role($selectedItem);	
+			        break;
+	*/
+			}
+	}
+	
 }
-add_action( 'gform_after_submission_7', 'manage_my_subscription', 10, 2 );
+add_action( 'gform_after_submission_14', 'manage_my_subscription', 10, 2 );
+
+
+
+// update '1' to the ID of your form
+add_filter( 'gform_pre_render_14', 'add_readonly_script' );
+function add_readonly_script( $form ) {
+    ?>
+ 
+    <script type="text/javascript">
+        jQuery(document).ready(function(){
+            /* apply only to a textarea with a class of gf_readonly */
+            jQuery('#input_7_7 option[value="none|0"]').attr("readonly","readonly");
+        });
+    </script>
+ 
+    <?php
+    return $form;
+}
+
+
+
+
 ?>
